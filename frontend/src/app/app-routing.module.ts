4001ce102b5c631efe0cdd3f475ec932ec44c984
MarioMS90 <mario.ms90@gmail.com>
2021-05-31 19:44:07 +0200

    publish

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
     component: ProfilePageComponent,
   },
   {
-    path: ':alias',
+    path: 'profile/:alias',
     canActivate: [AuthGuard],
     component: ProfilePageComponent,
   },
