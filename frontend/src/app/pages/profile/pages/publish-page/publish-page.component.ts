b2caa1911a739a6cc00bbe0e6f85c9306dc85a6b
Practicas <practicas@iMac-de-admin.home>
2021-06-01 10:38:40 +0200

    drag

--- frontend/src/app/pages/profile/pages/publish-page/publish-page.component.ts
+++ frontend/src/app/pages/profile/pages/publish-page/publish-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FileSystemDirectoryEntry, FileSystemFileEntry, NgxFileDropEntry } from 'ngx-file-drop';
 import { User } from 'src/app/shared/models/user.interface';
 import { UserService } from 'src/app/shared/services/user.service';
 
@@ -10,8 +11,9 @@ import { UserService } from 'src/app/shared/services/user.service';
 export class PublishPageComponent implements OnInit {
   public user: User;
   public isImageUploading = false;
+  public files: NgxFileDropEntry[] = [];
 
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
   ngOnInit() {
     this.userService.getUser().subscribe(user => {
@@ -19,10 +21,40 @@ export class PublishPageComponent implements OnInit {
     });
   }
 
-  onImageSelected(event) {
-    const image: File = event.target.files[0];
+  public dropped(files: NgxFileDropEntry[]) {
+    this.files = files;
+    for (const droppedFile of files) {
 
-    if (image) {
+      // Is it a file?
+      if (droppedFile.fileEntry.isFile) {
+        const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
+        fileEntry.file((file: File) => {
+
+          // Here you can access the real file
+          console.log(droppedFile.relativePath, file);
+
+          /**
+          // You could upload it like this:
+          const formData = new FormData()
+          formData.append('logo', file, relativePath)
+
+          // Headers
+          const headers = new HttpHeaders({
+            'security-token': 'mytoken'
+          })
+
+          this.http.post('https://mybackend.com/api/upload/sanitize-and-save-logo', formData, { headers: headers, responseType: 'blob' })
+          .subscribe(data => {
+            // Sanitized logo returned from backend
+          })
+          **/
+
+        });
+      } else {
+        // It was a directory (empty directories are added, otherwise only files)
+        const fileEntry = droppedFile.fileEntry as FileSystemDirectoryEntry;
+        console.log(droppedFile.relativePath, fileEntry);
+      }
     }
   }
 }
--- frontend/src/app/pages/profile/profile.module.ts
+++ frontend/src/app/pages/profile/profile.module.ts
@@ -4,12 +4,13 @@ import { SharedModule } from '../../shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { PublishPageComponent } from './pages/publish-page/publish-page.component';
+import { NgxFileDropModule } from 'ngx-file-drop';
 
 const declarations = [ProfilePageComponent, PublishPageComponent];
-const imports = [SharedModule, RouterModule, InfiniteScrollModule];
+const imports = [SharedModule, RouterModule, InfiniteScrollModule, NgxFileDropModule];
 
 @NgModule({
   declarations: declarations,
   imports: imports,
 })
-export class ProfileModule {}
+export class ProfileModule { }
